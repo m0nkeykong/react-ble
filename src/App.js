d83dc2edafb9cd879e75545c5f04ebddea2275d3
m0nkeykong-mac <haim1321@gmail.com>
2019-03-28 18:22:02 +0200

    first upload
    first upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,144 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './normalize.css';
+import BluetoothTerminal from './BLETerminal';
+
+// UI elements.
+// const deviceNameLabel = document.getElementById('device-name');
+// const connectButton = document.getElementById('connect');
+// const disconnectButton = document.getElementById('disconnect');
+// const terminalContainer = document.getElementById('terminal');
+// const sendForm = document.getElementById('send-form');
+// const inputField = document.getElementById('input');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.terminal = new BluetoothTerminal();
+    
+    // functions
+    this.receive = this.receive.bind(this);
+    this.logToTerminal = this.logToTerminal.bind(this);
+    this._log = this._log.bind(this);
+    this.scrollElement = this.scrollElement.bind(this);
+    this.send = this.send.bind(this);
+    this.connectButton = this.connectButton.bind(this);
+    this.disconnectButton = this.disconnectButton.bind(this);
+    this.submited = this.submited.bind(this);
+    this.scrolled = this.scrolled.bind(this);
+
+    // Helpers.
+    this.defaultDeviceName = 'Terminal';
+    this.terminalAutoScrollingLimit = 0;
+    this.isTerminalAutoScrolling = true;
+
+    
+    // Switch terminal auto scrolling if it scrolls out of bottom.
+  }
+
+  scrolled = () => {
+    const scrollTopOffset = this.refs.terminalContainer.scrollHeight -
+        this.refs.terminalContainer.offsetHeight - this.terminalAutoScrollingLimit;
+
+      this.isTerminalAutoScrolling = (scrollTopOffset < this.refs.terminalContainer.scrollTop);
+  };
+  
+  submited = (event) => {
+    //event.preventDefault();
+    this.send(this.refs.inputField.value);
+    this.refs.inputField.value = '';
+    this.refs.inputField.focus();
+  }
+
+  scrollElement = (element) => {
+    const scrollTop = element.scrollHeight - element.offsetHeight;
+
+    if (scrollTop > 0) {
+      element.scrollTop = scrollTop;
+    }
+  };
+
+  logToTerminal = (message, type = '') => {
+    this.refs.terminalContainer.insertAdjacentHTML('beforeend', `<div${type && ` class="${type}"`}>${message}</div>`);
+
+    if (this.isTerminalAutoScrolling) {
+      this.scrollElement(this.refs.terminalContainer);
+    }
+  };
+
+// Obtain configured instance.
+
+
+  // Override `receive` method to log incoming data to the terminal
+    receive = () => {
+      this.terminal.receive = (data) => {
+        this.logToTerminal(data, 'in');
+      };
+    }
+
+  // Override default log method to output messages to the terminal and console.
+    _log = () => {
+      this.terminal._log = function (...messages) {
+        // We can't use `super._log()` here.
+        messages.forEach((message) => {
+          this.logToTerminal(message);
+          console.log(message); // eslint-disable-line no-console
+        });
+      }
+    }
+
+  // Implement own send function to log outcoming data to the terminal.
+    send = (data) => {
+      this.terminal.send(data).
+        then(() => this.logToTerminal(data, 'out')).
+        catch((error) => this.logToTerminal(error));
+    };
+
+  // Bind event listeners to the UI elements.
+    connectButton = () => {
+      this.terminal.connect().
+        then(() => {
+          //this.refs.deviceNameLabel.textContent = this.terminal.getDeviceName() ? this.terminal.getDeviceName() : this.defaultDeviceName;
+        });
+    };
+
+    disconnectButton = () => {
+      this.terminal.disconnect();
+      //this.refs.deviceNameLabel.textContent = this.defaultDeviceName;
+    };
+
+    componentDidMount() {
+      this.refs.sendForm.addEventListener('submit', this.submited);
+      this.refs.terminalContainer.addEventListener('scroll', this.scrolled);
+      
+      this.terminalAutoScrollingLimit = this.refs.terminalContainer.offsetHeight / 2;
+    }
+
+
+  
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="toolbar">
+          <div id="device-name" className="name" ref="deviceNameLabel">Terminal</div>
+          <div className="buttons">
+            <button id="connect" onClick={this.connectButton} type="button" aria-label="Connect" ref="device-name">
+              <i className="material-icons">bluetooth_connected</i>
+            </button>
+            <button id="disconnect" onClick={this.disconnectButton} type="button" aria-label="Disconnect">
+              <i className="material-icons">bluetooth_disabled</i>
+            </button>
+          </div>
+        </div>
+        <div id="terminal" ref="terminalContainer" className="terminal"></div>
+        <form id="send-form" ref="sendForm" className="send-form">
+          <input id="input" ref="inputField" type="text" aria-label="Input" autoComplete="off" placeholder="Type something to send..."/>
+            <button type="submit" aria-label="Send">
+              <i className="material-icons">send</i>
+            </button>
+        </form>
       </div>
     );
   }
